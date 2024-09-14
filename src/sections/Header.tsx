b0a43194906fb0b403611e2599ab7ae049c2a9c7
bachisawsum <bachisawsum@gmail.com>
2024-09-14 14:50:30 +0000

    added content to hero section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,5 +1,5 @@
 export const Header = () => {
-  return <div className="flex justify-center items-center relative top-3">
+  return <div className="flex justify-center items-center fixed top-3">
     <nav className="flex gap-1 p-0.5 border boarder-white/15 rounded-full bg-white/10 backdrop-blur">
       <a href="#" className="nav-item">Home</a>
       <a href="#" className="nav-item">Projects</a>
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,29 @@
+import Image from "next/image";
+import memojiImage from "@/assets/images/memoji-computer.png"
+import ArrowDown from "@/assets/icons/arrow-down.svg"
+
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
+  return <div className="py-32">
+    <div className="container">
+      <div className="flex flex-col items-center">
+      <Image src={memojiImage} className="size-[100px]" alt="person peeking from laptop" />
+      <div className="bg-gray-950 boarder boarder-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
+        <div className="bg-green-500 size-2.5 rounded-full"></div>
+        <div className="text-sm font-medium">Available for projects</div>
+      </div>
+      </div>
+      <h1 className="font-serif text-3xl text-center mt-8 tracking-wide">Building Exeptional User Expriences</h1>
+      <p className="mt-4 text-center text-white/60">I specialise in transforming designs into functional, high-performing web applications. Let's discuss your next project.</p>
+      <div className="flex flex-col items-center mt-8 gap-4">
+        <button className="inline-flex items-center gap-2 border boarder-white/15 px-6 h-12 rounded-xl">
+          <span className="font-semibold">Expore My Work</span>
+          <ArrowDown className="size-4"/>
+        </button>
+        <button className="inline-flex items-center gap-2 boarder boarder-white bg-white text-gray-900 h-12 px-6 rounded-xl">
+          <span>👋🏻</span>
+          <span className="font-semibold">Let's Connect</span>
+        </button>
+      </div>
+    </div>
+  </div>;
 };
